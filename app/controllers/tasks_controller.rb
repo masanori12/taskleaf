68cd13bdbacdc238d5bd944672f0ca661ff05e9e
masanori12 <masadevi.m1218@gmail.com>
2022-04-02 12:34:40 +0900

    taskleaf chapter4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,47 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.all
-  end
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  def show
-    @task = Task.find(params[:id])
+  def index
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def new
     @task = Task.new
   end
 
+  def create
+    @task = current_user.tasks.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "タスク 「#{@task.name}」 を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
+    @task.update!(task_params)
     redirect_to tasks_url, notice: "タスク 「#{task.name}」 を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    @task.destroy
     redirect_to tasks_url, notice: "タスク 「#{task.name}」 を削除しました。"
-  end
-
-  def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク 「#{task.name}」 を登録しました。"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
